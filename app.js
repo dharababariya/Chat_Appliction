7858e0585db94ca78f13cde843b6c7716797673e
Dhara <dharababariya34@gmail.com>
2019-03-22 10:06:30 +0530

    create static file

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const express = require('express');
 //App setup
 const app = express();
 
-const server = app.listen(4000,()=>{console.log('listn port 4000')})
+const server = app.listen(4000,()=>{console.log('listn port 4000')});
+
+//static file
+
+app.use(express.static('public'))
